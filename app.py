49727507be1641e587f94f3acc81af17c9163bc2
Chaeeun Han <hance9908@gmail.com>
2024-11-29 09:29:58 +0900

    Update app.py, chat.py, config/conf.yaml, and document.py for prompt improvements and guide URL addition

--- app.py
+++ app.py
@@ -15,15 +15,17 @@ from document import Document_
 from chat import Chatbot_
 from exceptions import PineconeIndexNameError, PineconeUnexceptedException
 
-#*logging
+# *logging
 import logging
 from datetime import datetime, timezone, timedelta
+
 log_file_path = "api_requests.log"
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 file_handler = logging.FileHandler(log_file_path)
 file_handler.setLevel(logging.INFO)
-#formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+# formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
 
 class SeoulFormatter(logging.Formatter):
     def converter(self, timestamp):
@@ -31,7 +33,8 @@ class SeoulFormatter(logging.Formatter):
         dt = datetime.fromtimestamp(timestamp, tz=kst)
         return dt.timetuple()
 
-formatter = SeoulFormatter('%(asctime)s - %(levelname)s - %(message)s')
+
+formatter = SeoulFormatter("%(asctime)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
@@ -39,215 +42,287 @@ logger.addHandler(file_handler)
 load_dotenv()
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['JSON_AS_ASCII'] = False
-CORS(app) 
+app.config["DEBUG"] = True
+app.config["JSON_AS_ASCII"] = False
+CORS(app)
 
-api = Api(app, version='1.0', title='API Document',
-          description='Coach assistant Chatbot API Document',
-          doc='/docs/'
-         )
+api = Api(
+    app,
+    version="1.0",
+    title="API Document",
+    description="Coach assistant Chatbot API Document",
+    doc="/docs/",
+)
 
-ns_summary = api.namespace('summary', description='요약 API')
-ns_reference = api.namespace('reference', description='참고 문서 검색 API')
-ns_answer = api.namespace('answer', description='답변 생성 API')
+ns_summary = api.namespace("summary", description="요약 API")
+ns_reference = api.namespace("reference", description="참고 문서 검색 API")
+ns_answer = api.namespace("answer", description="답변 생성 API")
 
-summary_model = api.model('Summary', {
-    'query': fields.String(required=True, description='요약할 사용자의 질문 입력')
-})
+summary_model = api.model(
+    "Summary",
+    {"query": fields.String(required=True, description="요약할 사용자의 질문 입력")},
+)
 
-reference_model = api.model('Reference', {
-    'query': fields.String(required=True, description='답변을 생성하기 위해 필요한 문서를 검색하기 위해 질문 입력')
-})
+reference_model = api.model(
+    "Reference",
+    {
+        "query": fields.String(
+            required=True,
+            description="답변을 생성하기 위해 필요한 문서를 검색하기 위해 질문 입력",
+        )
+    },
+)
 
-answer_model = api.model('Answer', {
-    'query': fields.String(required=True, description='답변을 생성할 사용자의 질문 입력'),
-    'data': fields.List(fields.Raw(), required=True, description='검색된 문서의 인덱스와 내용을 리스트 형태로 입력')
-})
+answer_model = api.model(
+    "Answer",
+    {
+        "query": fields.String(
+            required=True, description="답변을 생성할 사용자의 질문 입력"
+        ),
+        "data": fields.List(
+            fields.Raw(),
+            required=True,
+            description="검색된 문서의 인덱스와 내용을 리스트 형태로 입력",
+        ),
+    },
+)
 
 llm = Chatbot_()
 document = Document_()
 
-@ns_summary.route('/')
+
+@ns_summary.route("/")
 class Summary(Resource):
     @api.expect(summary_model)
-    @api.response(200, 'Success')
-    @api.response(403, 'Remote API server error')
-    @api.response(405, 'Query is required')
-    @api.response(500, 'Internal Server Error')
+    @api.response(200, "Success")
+    @api.response(403, "Remote API server error")
+    @api.response(405, "Query is required")
+    @api.response(500, "Internal Server Error")
     def post(self):
         try:
             data = request.json
-            query = data['query']
+            query = data["query"]
 
             if not query.strip():
-                return make_response(jsonify({
-                    "status_code": 405,
-                    "message": "쿼리를 입력해주세요.",
-                    "error": "ValueError: Empty query"
-                    }), 405)
+                return make_response(
+                    jsonify(
+                        {
+                            "status_code": 405,
+                            "message": "쿼리를 입력해주세요.",
+                            "error": "ValueError: Empty query",
+                        }
+                    ),
+                    405,
+                )
 
             logger.info(f"query: {query}")
             summary = llm.summary(query)
 
-            return jsonify({
-                "status_code": 200,
-                "data": [
-                    {
-                        "summary": summary,
-                    }
-                ]
-            })
+            return jsonify(
+                {
+                    "status_code": 200,
+                    "data": [
+                        {
+                            "summary": summary,
+                        }
+                    ],
+                }
+            )
 
         except openai.APIError as e:
-            return make_response(jsonify({
-                "status_code": 403,
-                "message": "현재 AI 질문 요약이 어렵습니다. 잠시 후에 다시 사용해주세요.",
-                "error": "OpenaiApiKeyError: Invalid OpenAI API Key"
-                }), 403)
-        
+            return make_response(
+                jsonify(
+                    {
+                        "status_code": 403,
+                        "message": "현재 AI 질문 요약이 어렵습니다. 잠시 후에 다시 사용해주세요.",
+                        "error": "OpenaiApiKeyError: Invalid OpenAI API Key",
+                    }
+                ),
+                403,
+            )
+
         except Exception as e:
-            return make_response(jsonify({
-                "status_code": 500,
-                "message": "현재 AI 질문 요약이 어렵습니다. 잠시 후에 다시 사용해주세요.",
-                "error": f"UnexpectedError: {traceback.format_exc()}"
-                }), 403)
-        
-@ns_reference.route('/')
+            return make_response(
+                jsonify(
+                    {
+                        "status_code": 500,
+                        "message": "현재 AI 질문 요약이 어렵습니다. 잠시 후에 다시 사용해주세요.",
+                        "error": f"UnexpectedError: {traceback.format_exc()}",
+                    }
+                ),
+                403,
+            )
+
+
+@ns_reference.route("/")
 class Reference(Resource):
     @api.expect(reference_model)
-    @api.response(200, 'Success')
-    @api.response(204, 'No relevant guide available')
-    @api.response(403, 'Remote API server error')
-    @api.response(405, 'Query is required')
-    @api.response(500, 'Internal Server Error')
+    @api.response(200, "Success")
+    @api.response(204, "No relevant guide available")
+    @api.response(403, "Remote API server error")
+    @api.response(405, "Query is required")
+    @api.response(500, "Internal Server Error")
     def post(self):
         try:
             data = request.json
-            query = data['query']
+            query = data["query"]
 
             if not query.strip():
-                return make_response(jsonify({
-                    "status_code": 405,
-                    "message": "쿼리를 입력해주세요.",
-                    "error": "ValueError: Empty query"
-                    }), 405)
-            
+                return make_response(
+                    jsonify(
+                        {
+                            "status_code": 405,
+                            "message": "쿼리를 입력해주세요.",
+                            "error": "ValueError: Empty query",
+                        }
+                    ),
+                    405,
+                )
+
             context = document.find_match(query)
 
             if not all(list(zip(*context))[0]):
-                return make_response(jsonify({
-                    "status_code": 204,
-                    "date": [
-                        {
-                            "reference": []
-                        }
-                    ]
-                }), 204)
+                return make_response(
+                    jsonify({"status_code": 204, "date": [{"reference": []}]}), 204
+                )
 
-            reference = {"reference": []} 
+            reference = {"reference": []}
             for c in context:
-                keywords_with_newline = [k + '\n' for k in c[1]] 
-                reference["reference"].append({
-                    "index": c[0],
-                    "keyword": keywords_with_newline,  
-                    "text": c[2],
-                })
+                keywords_with_newline = [k + "\n" for k in c[1]]
+                reference["reference"].append(
+                    {
+                        "index": c[0],
+                        "keyword": keywords_with_newline,
+                        "text": c[2],
+                        "image_url": c[3],
+                    }
+                )
 
-            return jsonify({
-                "status_code": 200,
-                "data": [
-                    reference
-                ]
-            })
+            return jsonify({"status_code": 200, "data": [reference]})
 
         except openai.APIError as e:
-            return make_response(jsonify({
-                "status_code": 403,
-                "message": "현재 AI 답변 가이드 검색이 어렵습니다. 잠시 후에 다시 사용해주세요.",
-                "error": "OpenaiApiKeyError: Invalid OpenAI API Key"
-                }), 403)
-        
+            return make_response(
+                jsonify(
+                    {
+                        "status_code": 403,
+                        "message": "현재 AI 답변 가이드 검색이 어렵습니다. 잠시 후에 다시 사용해주세요.",
+                        "error": "OpenaiApiKeyError: Invalid OpenAI API Key",
+                    }
+                ),
+                403,
+            )
+
         except pinecone.exceptions.PineconeApiException as e:
-            return make_response(jsonify({
-                "status_code": 403,
-                "message": "현재 AI 답변 가이드 검색이 어렵습니다. 잠시 후에 다시 사용해주세요.",
-                "error": "PineconeApiKeyError: Invalid Pinecone API Key"
-                }), 403)
-        
+            return make_response(
+                jsonify(
+                    {
+                        "status_code": 403,
+                        "message": "현재 AI 답변 가이드 검색이 어렵습니다. 잠시 후에 다시 사용해주세요.",
+                        "error": "PineconeApiKeyError: Invalid Pinecone API Key",
+                    }
+                ),
+                403,
+            )
+
         except PineconeIndexNameError as e:
-            return make_response(jsonify({
-                "status_code": 403,
-                "message": "현재 AI 답변 가이드 검색이 어렵습니다. 잠시 후에 다시 사용해주세요.",
-                "error": "PineconeIndexNameError: Pinecone index does not exist"
-                }), 403)
-        
+            return make_response(
+                jsonify(
+                    {
+                        "status_code": 403,
+                        "message": "현재 AI 답변 가이드 검색이 어렵습니다. 잠시 후에 다시 사용해주세요.",
+                        "error": "PineconeIndexNameError: Pinecone index does not exist",
+                    }
+                ),
+                403,
+            )
+
         except PineconeUnexceptedException as e:
-            return make_response(jsonify({
-                "status_code": 500,
-                "message": "현재 AI 답변 가이드 검색이 어렵습니다. 잠시 후에 다시 사용해주세요.",
-                "error": e
-                }), 500)
-        
+            return make_response(
+                jsonify(
+                    {
+                        "status_code": 500,
+                        "message": "현재 AI 답변 가이드 검색이 어렵습니다. 잠시 후에 다시 사용해주세요.",
+                        "error": e,
+                    }
+                ),
+                500,
+            )
+
         except Exception as e:
-            return make_response(jsonify({
-                "status_code": 500,
-                "message": "현재 AI 답변 가이드 검색이 어렵습니다. 잠시 후에 다시 사용해주세요.",
-                "error": f"UnexpectedError: {traceback.format_exc()}"
-                }), 500)
+            return make_response(
+                jsonify(
+                    {
+                        "status_code": 500,
+                        "message": "현재 AI 답변 가이드 검색이 어렵습니다. 잠시 후에 다시 사용해주세요.",
+                        "error": f"UnexpectedError: {traceback.format_exc()}",
+                    }
+                ),
+                500,
+            )
+
 
 # Answer 리소스 클래스 정의
-@ns_answer.route('/')
+@ns_answer.route("/")
 class Answer(Resource):
     @api.expect(answer_model)
-    @api.response(200, 'Success')
-    @api.response(403, 'Remote API server error')
-    @api.response(405, 'Query is required')
-    @api.response(500, 'Internal Server Error')
+    @api.response(200, "Success")
+    @api.response(403, "Remote API server error")
+    @api.response(405, "Query is required")
+    @api.response(500, "Internal Server Error")
     def post(self):
         try:
             json_data = request.json
-            query = json_data['query']
-            reference_list = json_data['data'][0]['reference']
+            query = json_data["query"]
+            reference_list = json_data["data"][0]["reference"]
 
             if not query.strip():
-                return make_response(jsonify({
-                    "status_code": 405,
-                    "message": "쿼리를 입력해주세요.",
-                    "error": "ValueError: Empty query"
-                    }), 405)
+                return make_response(
+                    jsonify(
+                        {
+                            "status_code": 405,
+                            "message": "쿼리를 입력해주세요.",
+                            "error": "ValueError: Empty query",
+                        }
+                    ),
+                    405,
+                )
 
             context = document.context_to_string(reference_list, query)
 
             if context:
-                reference=context
+                reference = context
             else:
-                reference=['참고문서는 없으니 너가 아는 정보로 대답해줘.']
-            answer = llm.getConversation_prompttemplate(query=query, reference=reference)
+                reference = ["참고문서는 없으니 너가 아는 정보로 대답해줘."]
+            answer = llm.getConversation_prompttemplate(
+                query=query, reference=reference
+            )
 
-            return jsonify({
-                "status_code": 200,
-                "data": [
-                    {
-                        "answer": answer
-                    }
-                ]
-            })
+            return jsonify({"status_code": 200, "data": [{"answer": answer}]})
 
         except openai.APIError as e:
-            return make_response(jsonify({
-                "status_code": 403,
-                "message": "현재 AI 답변 추천이 어렵습니다. 잠시 후에 다시 사용해주세요.",
-                "error": "OpenaiApiKeyError: Invalid OpenAI API Key"
-                }), 403)
+            return make_response(
+                jsonify(
+                    {
+                        "status_code": 403,
+                        "message": "현재 AI 답변 추천이 어렵습니다. 잠시 후에 다시 사용해주세요.",
+                        "error": "OpenaiApiKeyError: Invalid OpenAI API Key",
+                    }
+                ),
+                403,
+            )
 
         except Exception as e:
-            return make_response(jsonify({
-                "status_code": 500,
-                "message": "현재 AI 답변 추천이 어렵습니다. 잠시 후에 다시 사용해주세요.",
-                "error": f"UnexpectedError: {traceback.format_exc()}"
-                }), 500)
+            return make_response(
+                jsonify(
+                    {
+                        "status_code": 500,
+                        "message": "현재 AI 답변 추천이 어렵습니다. 잠시 후에 다시 사용해주세요.",
+                        "error": f"UnexpectedError: {traceback.format_exc()}",
+                    }
+                ),
+                500,
+            )
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- document.py
+++ document.py
@@ -15,58 +15,66 @@ from utils import query_refiner
 
 
 load_dotenv()
-with open(os.path.join(os.path.dirname(__file__), "config", 'conf.yaml')) as f:
+with open(os.path.join(os.path.dirname(__file__), "config", "conf.yaml")) as f:
     config = yaml.full_load(f)
 
 pc = Pinecone()
 index = pc.Index(config["pinecone"]["index_name"])
 
 model = AutoModel.from_pretrained(config["embedding_model"]["model_path"])
-tok = AutoTokenizer.from_pretrained(config["embedding_model"]["model_path"], clean_up_tokenization_spaces=True)
+tok = AutoTokenizer.from_pretrained(
+    config["embedding_model"]["model_path"], clean_up_tokenization_spaces=True
+)
 
-vectorizer = pk.load(open(os.path.join(os.path.dirname(__file__), "config", "params", "tfidf_params.pkl"), "rb"))
+vectorizer = pk.load(
+    open(
+        os.path.join(os.path.dirname(__file__), "config", "params", "tfidf_params.pkl"),
+        "rb",
+    )
+)
 
 
 class Document_:
     def __init__(self):
         pass
-    
-    def _sentence_embedding(self, query:str) -> List[float]:
-        inputs = tok(query, return_tensors="pt", padding=True, truncation=True, max_length=512)
-        
+
+    def _sentence_embedding(self, query: str) -> List[float]:
+        inputs = tok(
+            query, return_tensors="pt", padding=True, truncation=True, max_length=512
+        )
+
         with torch.no_grad():
             outputs = model(**inputs)
-        
+
         embeddings = outputs.last_hidden_state
         attention_mask = inputs["attention_mask"]
         mask = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
-        mean_pooling_embedding = torch.sum(embeddings * mask, 1) / torch.clamp(mask.sum(1), min=1e-9)
+        mean_pooling_embedding = torch.sum(embeddings * mask, 1) / torch.clamp(
+            mask.sum(1), min=1e-9
+        )
 
         mean_pooling_embedding = normalize(mean_pooling_embedding, norm="l2")
 
         return mean_pooling_embedding.reshape(-1).tolist()
 
-    def _tfidf_sparse_vector(self, query:str) -> Tuple[List[int], List[float]]:
+    def _tfidf_sparse_vector(self, query: str) -> Tuple[List[int], List[float]]:
         query_tfidf = vectorizer.transform([query])
 
         indices = query_tfidf.nonzero()[1].tolist()
         values = query_tfidf.data.tolist()
-        return {
-            "indices": indices,
-            "values": values
-        }
+        return {"indices": indices, "values": values}
 
     def context_to_string(self, contexts, query):
-            context = '\n'.join(contexts)
-            if len(context) > 2000:
-                context = context[:2000]
-            if (len(context + query)) > 2500:
-                context = context[:2500 - len(query)]
-            return context
+        context = "\n".join(contexts)
+        if len(context) > 2000:
+            context = context[:2000]
+        if (len(context + query)) > 2500:
+            context = context[: 2500 - len(query)]
+        return context
 
     def query_refine(self, query):
         return query_refiner(query)
-    
+
     def find_match(self, query):
         embed_query = self._sentence_embedding(query=query)
         sparse_vector = self._tfidf_sparse_vector(query=query)
@@ -75,17 +83,13 @@ class Document_:
             result = index.query(
                 vector=embed_query,
                 sparse_vector=sparse_vector,
-                top_k=10, 
-                include_metadata=True
+                top_k=10,
+                include_metadata=True,
             )
 
             threshold = 0.25
         else:
-            result = index.query(
-                vector=embed_query,
-                top_k=10, 
-                include_metadata=True
-            )
+            result = index.query(vector=embed_query, top_k=10, include_metadata=True)
 
             threshold = 0.3
 
@@ -95,16 +99,17 @@ class Document_:
         for res in result.matches:
             r = []
             if res.score >= threshold:
-                reference_id = res['id']
+                reference_id = res["id"]
 
                 keywords = res["metadata"]["keywords"]
                 answer = res["metadata"]["text"]
+                image_url = res["metadata"]["url"]
 
-                r = [reference_id, keywords, answer]
+                r = [reference_id, keywords, answer, image_url]
 
             else:
                 reference_id = None
-                r = [reference_id, [], []]
+                r = [reference_id, [], [], []]
 
             ref_list.append(r)
 
