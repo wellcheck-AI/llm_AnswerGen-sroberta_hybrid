5821319cb7d1991139854fcd295c38e4c3f0725a
dyk4ever0 <dyk4ever0@gmail.com>
2024-12-03 16:37:06 +0900

    __init__ utils

--- testmacab.py
+++
@@ -1,9 +0,0 @@
-from konlpy.tag import Mecab
-
-dicpath = "/opt/homebrew/lib/mecab/dic/mecab-ko-dic"
-try:
-    mecab = Mecab(dicpath=dicpath)
-    print(f'성공! 사용된 사전 경로: {dicpath}')
-    print('테스트 결과:', mecab.pos('테스트 문장입니다.'))
-except Exception as e:
-    print(f'오류 발생: {str(e)}')
